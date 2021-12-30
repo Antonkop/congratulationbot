0691aba703f61e06456d50e55e1915eb5552c4f5
anton <VTB4050847@region.vtb.ru>
2021-12-30 20:48:11 +0400

    Initial commit

---
+++ src/main/kotlin/com/kop/congratulationbot/CongratulationbotApplication.kt
@@ -0,0 +1,11 @@
+package com.kop.congratulationbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CongratulationbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<CongratulationbotApplication>(*args)
+}
---
+++ src/test/kotlin/com/kop/congratulationbot/CongratulationbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kop.congratulationbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CongratulationbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
